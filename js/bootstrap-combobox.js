76d7b5a17bfaaad5ed2296b321d85bf60a900e2b
Bernd Bestel <bernd@berrnd.de>
2026-07-21 21:23:56 +0200

    Fixed dropdown caret / remove icon handling

--- js/bootstrap-combobox.js
+++ js/bootstrap-combobox.js
@@ -187,16 +187,14 @@
         return '<div class="combobox-container"><input type="hidden" /> <div class="input-append"> <input type="text" autocomplete="off" /> <span class="add-on dropdown-toggle" data-dropdown="dropdown"> <span class="caret pulldown" style="vertical-align: middle"/> <i class="icon-remove remove"/> </span> </div> </div>'
       } else if (this.options.bsVersion == '3') {
         return '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <span class="input-group-addon dropdown-toggle" data-dropdown="dropdown"> <span class="caret pulldown" /> <span class="glyphicon glyphicon-remove remove" /> </span> </div> </div>'
-      } else if (this.options.bsVersion == '4') {
-        return '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <div class="input-group-append"> <span class="input-group-text dropdown-toggle" data-toggle="dropdown"></span> </div> </div>'
       } else {
         return '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" />'
-          + '<span class="input-group-append"' + (hasPopper ? ' data-toggle="dropdown" data-reference="parent"' : '') + '>'
-            + '<span class="input-group-text dropdown-toggle' + (this.options.iconCaret ? ' custom-icon' : '') + '">'
-              + (this.options.iconCaret ? '<span class="' + this.options.iconCaret + ' pulldown" />' : '')
-              + (this.options.iconRemove ? '<span class="' + this.options.iconRemove + ' remove" />' : '<span class="utf-remove remove" />')
-            + '</span>'
-          + '</span> </div> </div>';
+          + '<div class="input-group-append"' + (hasPopper ? ' data-toggle="dropdown" data-reference="parent"' : '') + '>'
+            + '<div class="input-group-text dropdown-toggle' + (this.options.iconCaret ? ' custom-icon' : '') + '" data-toggle="dropdown">'
+              + (this.options.iconCaret ? '<span class="' + this.options.iconCaret + ' pulldown"></span>' : '')
+              + (this.options.iconRemove ? '<span class="' + this.options.iconRemove + ' remove"></span>' : '<span class="utf-remove remove"></span>')
+            + '</div>'
+          + '</div> </div> </div>';
       }
     }
 
